4b87c3d4ebd735b682756a7df8a9e967b212b94e
Toby Schachman <tobyschachman@gmail.com>
2012-12-05 16:21:11 -0800

    added matchbrackets to codemirror

--- dev/components/codemirror/index.js
+++ dev/components/codemirror/index.js
@@ -2,5 +2,6 @@ var codemirror = require("./codemirror");
 
 require("./glsl")(codemirror);
 require("./runmode")(codemirror);
+require("./matchbrackets")(codemirror);
 
 module.exports = codemirror;
