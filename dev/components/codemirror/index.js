b894e272745733ed5484cb58a93d67da93ba7c11
Toby Schachman <tobyschachman@gmail.com>
2012-12-11 14:45:37 -0800

    moved stuff around, using "paths" in component

--- dev/components/codemirror/index.js
+++
@@ -1,7 +0,0 @@
-var codemirror = require("./codemirror");
-
-require("./glsl")(codemirror);
-require("./runmode")(codemirror);
-require("./matchbrackets")(codemirror);
-
-module.exports = codemirror;
